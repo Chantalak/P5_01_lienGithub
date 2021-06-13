b00662d2fa0d260d6fd56b1f8a6790770e5ec678
Chantalak <76796041+Chantalak@users.noreply.github.com>
2021-06-13 07:54:06 +0200

    add premier code javascript

--- FRONTEND/index.js
+++ FRONTEND/index.js
@@ -1,12 +1,30 @@
+const affichageName = document.querySelector("#name");
+const affichagePrice = document.querySelector("#price");
+const affichageDescription = document.querySelector("#description");
+
+console.log(affichagePrice)
+
 fetch ("http://localhost:3000/api/cameras")
-  .then(function(res) {
+
+  .then((res) => {
     if (res.ok) {
       return res.json();
     }
   })
-  .then(function(value) {
-    console.log(value);
+  .then((value) => {
+    console.log(value[0]);
+
+    const cameraName = value[0].name;
+    const cameraPrice = value[0].price;
+    const cameraDescription = value[0].description;
+
+    affichageName.innerHTML = cameraName;
+    affichagePrice.innerHTML = cameraPrice;
+    affichageDescription.innerHTML = cameraDescription;
+    
+    
   })
   .catch(function(err) {
-    // Une erreur est survenue
+    console.log(err) 
   });
+
