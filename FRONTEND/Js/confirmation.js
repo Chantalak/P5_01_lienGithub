7e8370707d9cdcc60778350a73cde94dd430b958
Chantalak <76796041+Chantalak@users.noreply.github.com>
2021-07-19 02:38:02 +0200

    création page confirmation html et js

---
+++ FRONTEND/Js/confirmation.js
@@ -0,0 +1,8 @@
+
+//RECUPERATION DES DONNEES CONTACT
+let contact = JSON.parse(localStorage.getItem("contact"));
+console.log(contact)
+
+// RECUPERATION DU PRIX TOTAL
+let prixTotal = JSON.parse(localStorage.getItem("prixTotal"));
+console.log(prixTotal);
