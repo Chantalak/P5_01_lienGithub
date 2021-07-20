b26a1e0c33a37999afa1e71340e3f320dda25045
Chantalak <76796041+Chantalak@users.noreply.github.com>
2021-07-20 23:31:38 +0200

    add validation formulaire mais ne fonctionne pas

--- FRONTEND/Js/confirmation.js
+++ FRONTEND/Js/confirmation.js
@@ -1,19 +1,34 @@
-//RECUPERATION DES DONNEES CONTACT
+
+//Récupération des données du contact
 let contact = JSON.parse(localStorage.getItem("contact"));
 console.log(contact)
 
-// RECUPERATION DU PRIX TOTAL
+//Récupération du prix total du panier 
 let prixTotal = JSON.parse(localStorage.getItem("prixTotal"));
-console.log(prixTotal);
 
-// RECUPERATION DE ORDERID
+//Récupération de orderId
 let orderId = JSON.parse(localStorage.getItem("orderId"));
-console.log(orderId);
 
-var merci = document.querySelector(".merci");
-console.log(merci)
-merci.textContent = "Bonjour " + contact.lastName + " " + contact.firstName + " nous vous remercions pour votre commande et espérons que vous avez apprécié votre shopping sur notre site.";
+//Affichage du message de confimation
+function displayThanksMessage() {
+    var merci = document.querySelector(".merci");
+    console.log(merci)
+    merci.textContent = "Bonjour " + contact.lastName + " " + contact.firstName + " nous vous remercions pour votre commande et espérons que vous avez apprécié votre shopping sur notre site.";
 
-var recap = document.querySelector(".recap");
-console.log(recap)
-recap.textContent = "Vous recevrez un e-mail de confirmation de votre commande : " + orderId + " d'un montant total de " + prixTotal;
+    var recap = document.querySelector(".recap");
+    console.log(recap)
+    recap.textContent = "Vous recevrez un e-mail de confirmation de votre commande : " + orderId + " d'un montant total de " + prixTotal;
+}
+
+//Appeler la fonction 
+displayThanksMessage(); 
+
+//Suppression du localstorage 
+var accueil = document.querySelector("#btn-confirmation");
+console.log(accueil)
+accueil.addEventListener('click', (e) =>{
+    localStorage.removeItem("element");
+    localStorage.removeItem("contact");
+    localStorage.removeItem("prixTotal");
+    localStorage.removeItem("orderId");
+})
